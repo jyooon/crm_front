6399eb73c89c5f26f43a39301463ea55b91d4a21
jyooon <innonext@localhost>
2019-02-27 02:21:46 +0900

    init

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './common/css/base.css'
+import { store } from './store/store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
